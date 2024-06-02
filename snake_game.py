6d795ee321bc9ee3a250037bc584b06be9a3d23f
imharshag <harshag3106@gmail.com>
2024-06-02 23:50:28 +0530

    test

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,31 @@
 import pygame
 import random
 
+pygame.init()
+
+class SnakeGame:
+
+    def __init__(self,w=640,h=480) #screen size in pixels
+        self.w=w
+        self.h=h
+
+        #init display
+
+        #init game state
+
+        def play_step(self):
+            pass
+
+
+
 if__name__=='__main__':
 game= SnakeGame()
  
 while True:
     gane.plat_step()
-    
+
+
+    #break if game over
+
+    pygame.quit()
+
